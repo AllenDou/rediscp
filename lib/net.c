a22b75405690ad4dc80157b217f9aa5345053e7f
Allen.Dou <allen.dou@hotmail.com>
2013-04-20 10:35:22 +0800

    update.

--- lib/net.c
+++ lib/net.c
@@ -114,30 +114,32 @@ static int redisSetBlocking(redisContext *c, int fd, int blocking) {
 }
 
 static int redisKeepAlive(redisContext *c,int fd,int interval) {
-
-	/*keepalive*/
-	int val = 1;
+    int val = 1;
 	
     if (setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, &val, sizeof(val)) == -1){
         __redisSetError(c,REDIS_ERR_OTHER,strerror(errno));
         return REDIS_ERR;
     }
+
     val = interval;
     if (setsockopt(fd, IPPROTO_TCP, TCP_KEEPIDLE, &val, sizeof(val)) < 0) {
         __redisSetError(c,REDIS_ERR_OTHER,strerror(errno));
         return REDIS_ERR;
     }
+
     val = interval/3;
     if (val == 0) val = 1;
     if (setsockopt(fd, IPPROTO_TCP, TCP_KEEPINTVL, &val, sizeof(val)) < 0) {
         __redisSetError(c,REDIS_ERR_OTHER,strerror(errno));
         return REDIS_ERR;
     }
+
     val = 3;
     if (setsockopt(fd, IPPROTO_TCP, TCP_KEEPCNT, &val, sizeof(val)) < 0) {
         __redisSetError(c,REDIS_ERR_OTHER,strerror(errno));
         return REDIS_ERR;
     }
+
     return REDIS_OK;
 }
 
